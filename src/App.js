852b07d4174baf63d8224a9d01d8ab179eee972a
Arthur Medeiros Paiva <56327949+ArthurOnly@users.noreply.github.com>
2020-08-12 14:30:27 -0300

    TextFields test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
+
+import "./styles.css"
+
+import { TextField, MenuItem, Stepper } from "@material-ui/core"
 
 function App() {
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = data => console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div id="app">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="app-content"
+        id="app-content"
+      >
+        <TextField
+          type="name"
+          helperText="type any"
+          size="small"
+          variant="filled"
+          label="label"
+          ref={register}
+          name="name"
+        ></TextField>
+        <TextField
+          type="name"
+          helperText="type any"
+          size="small"
+          variant="standard"
+          label="label"
+          ref={register}
+          name="name1"
+        ></TextField>
+        <TextField
+          type="name"
+          helperText="type any"
+          size="small"
+          variant="outlined"
+          label="label"
+          ref={register}
+          name="name2"
+        ></TextField>
+
+        <TextField
+          type="name"
+          helperText="type any"
+          size="small"
+          variant="filled"
+          label="label"
+          select
+          ref={register}
+          name="name3"
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value={1}>Teste teste</MenuItem>
+          <MenuItem value={2}>Teste teste2</MenuItem>
+        </TextField>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
